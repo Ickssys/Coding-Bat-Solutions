e0aa58f5e867ad63a888bb8c130e3f97e4f53278
Ickssys <beboldb@hotmail.com>
2023-03-21 22:01:46 -0400

    new files

--- Warmup-1/sleepIn.java
+++ Warmup-1/sleepIn.java
@@ -1,9 +1,6 @@
-public class sleepIn{
-
-	public boolean sleepIn(boolean weekday, boolean vacation){
-		if(!weekday || vacation){
-			return true;
-		return false;
+public boolean sleepIn(boolean weekday, boolean vacation){
+	if(!weekday || vacation){
+		return true;
+	return false;
 		}
 	}
-}
